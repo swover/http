adfb8b37c37679038ace7b0b350b5aeeca00be36
ruesin <ruesin@gmail.com>
2019-08-31 15:09:01 +0800

    parse handler

--- src/HttpFactory.php
+++ src/HttpFactory.php
@@ -15,19 +15,13 @@ class HttpFactory
 
     public static function setHandler($handler)
     {
-        if (is_object($handler) && $handler instanceof BaseClient) {
-            return self::$handler = $handler; //get_class($handler);
-        }
-
-        if ($handler == 'curl') return self::$handler = '\Swover\Http\Client\Curl';
-
-        if ($handler == 'guzzle') return self::$handler = '\Swover\Http\Client\Guzzle';
-
-        return self::$handler = false;
+        return self::$handler = self::parseHandler($handler);
     }
 
     public static function getHandler($default = false)
     {
+        $default = self::parseHandler($default);
+
         if ($default !== false) return $default;
 
         if (self::$handler !== false) return self::$handler;
@@ -43,6 +37,19 @@ class HttpFactory
         if (extension_loaded('curl')) return '\Swover\Http\Client\Curl';
 
         return '\Swover\Http\Client\Stream';
+    }
+
+    private static function parseHandler($handler)
+    {
+        if (is_object($handler) && $handler instanceof BaseClient) {
+            return $handler; //get_class($handler);
+        }
+
+        if ($handler == 'curl') return '\Swover\Http\Client\Curl';
+
+        if ($handler == 'guzzle') return '\Swover\Http\Client\Guzzle';
+
+        return false;
     }
 
     public static function setProxy($handler)
